69e73e12ebcf436e117a549d0ebbbcef1ab55943
davidpinillos5 <pinillosdavid5@gmail.com>
2020-11-23 16:09:15 +0100

    creacion de componentes de los videojuegos y enlaces de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { callbackify } from 'util';
+import { CallOfDutyComponent } from './call-of-duty/call-of-duty.component';
+import { FortniteComponent } from './fortnite/fortnite.component';
+import { HomeComponent } from './home/home.component';
+import { LeagueOfLegendsComponent } from './league-of-legends/league-of-legends.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo:'/home' },
+  { path: 'home', component: HomeComponent },
+  { path:'lol', component:LeagueOfLegendsComponent },
+  { path:'cod', component:CallOfDutyComponent },
+  { path:'fortnite', component:FortniteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormularioGeneralComponent } from './formulario-general/formulario-general.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LeagueOfLegendsComponent } from './league-of-legends/league-of-legends.component';
+import { HomeComponent } from './home/home.component';
+import { CallOfDutyComponent } from './call-of-duty/call-of-duty.component';
+import { FortniteComponent } from './fortnite/fortnite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FormularioGeneralComponent
+    FormularioGeneralComponent,
+    LeagueOfLegendsComponent,
+    HomeComponent,
+    CallOfDutyComponent,
+    FortniteComponent
   ],
   imports: [
     BrowserModule,
