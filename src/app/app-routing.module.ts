d9da138b5dc255e6db7351522e63a40fd0d5de14
davidpinillos5 <pinillosdavid5@gmail.com>
2020-11-30 15:02:35 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormularioGeneralComponent } from './formulario-general/formulario-general.component';
 import { HomeComponent } from './home/home.component';
 import { JuegoComponent } from './juego/juego.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  { path: 'juego/lol', component: JuegoComponent },
-  { path: 'juego/cod', component: JuegoComponent },
-  { path: 'juego/fortnite', component: JuegoComponent }
+  { path: 'home/juego', component: JuegoComponent },
+
+  { path: 'registro', component: FormularioGeneralComponent}
 ];
 
 @NgModule({
