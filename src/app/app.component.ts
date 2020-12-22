dda3597b5f5465e59e9053fb94650b510f1948bc
carlossalvadordiaz <csd95@hotmail.com>
2020-12-22 11:43:24 +0100

    Carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'duoTracker';
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#21242e'
+  }
 }
+
+/* export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef){
+
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'yourColor';
+ }
+}
+ */
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule, WavesModule } from 'angular-bootstrap-md'
+import swal from 'sweetalert2';
+import { PartidasService } from '../servicios/partidas.service';
+import { UsuariosService } from '../servicios/usuarios.service';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+
+  constructor(private partidasservice: PartidasService, private usuariosservice: UsuariosService) { }
+
+  ngOnInit(): void {
+    this.usuariosservice.getUsuario()
+    this.usuariosservice.isLogged()
+
+
+    if (!this.usuariosservice.isLogged()) {
+      setTimeout(() => {
+        swal.fire({
+          title: '¿Por qué no empiezas por registrarte?',
+          text: 'Regístrate y empieza a jugar hoy mismo',
+          imageUrl: 'https://media.giphy.com/media/XCmcHcwMnHHbwI1ea2/giphy-downsized.gif',
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: 'Custom image',
+        }).then(function () {
+          window.location.href = "/registro"
+        })
+      }, 3000);
+    }
+    /* setTimeout(() => {
+      swal.fire({
+        title: '¿A qué esperas para empezar??',
+        text: 'Ver tus juegos favoritos',
+        imageUrl: 'https://media.giphy.com/media/KHbfOPypfweSHuP0c1/giphy.gif',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      }).then(function () {
+        window.scrollTo(10, document.body.scrollHeight)
+      })
+    }, 3000); */
+  }
+
+
+  isLogged() {
+    if (this.usuariosservice.isLogged())
+
+      return true
+
+    else {
+      return false
+    }
+
+
+
+  }
+
+  onClick() {
+
+    window.scrollTo(0, document.body.scrollHeight)
+
+  }
+}
